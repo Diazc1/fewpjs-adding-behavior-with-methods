89f255bf2d91bd1af7b2ed8014ea3c62544a3bb1
Cathy Diaz <Cathydiaz14@gmail.com>
2021-07-08 04:54:42 -0700

    commit tree b54a6314d51e17cf201df28896b79c5c0a22628f

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+// cat
+// ✓ has a class of cat
+// ✓ cat.speak() returns '[name]' says meow!
+class Cat {
+    constructor(name) {
+    this.name = name;
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+
+// dog
+// ✓ has a class of dog
+// ✓ dog.speak() returns '[name] says woof!'
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex =  sex;
+    }
+
+    speak() {
+        if (this.sex == "male") {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
+
